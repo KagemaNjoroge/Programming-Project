5c8457be4dc686538ead984c83282f5de8c0d10b
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-10 20:31:13 +0300

    feat: Add create new poll page and poll list view

--- voting/urls.py
+++ voting/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import vote, mine_transactions, allocate_votes_view, voting_template_view, home
+
+from .views import vote, mine_transactions, allocate_votes_view, home, create_new_poll_page, PollListView
+
 app_name = "voting"
 urlpatterns = [
     path("vote/<int:poll_id>/", vote, name="vote"),
     path("mine/", mine_transactions, name="mine_transactions"),
     path("allocate/<int:poll_id>/", allocate_votes_view, name="allocate_votes"),
     path("", home, name="home"),
+    path("create/", create_new_poll_page, name="create_new_poll"),
+    path("polls/", PollListView.as_view(), name="polls"),
+
 ]
