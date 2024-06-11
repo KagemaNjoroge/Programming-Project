5943fa4189ec442e482b2a10239575545c3b49e1
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-11 15:33:35 +0300

    chore: Update voting/urls.py for new views and paths

--- voting/urls.py
+++ voting/urls.py
@@ -1,14 +1,35 @@
 from django.urls import path
 
-from .views import vote, mine_transactions, allocate_votes_view, home, create_new_poll_page, PollListView
+from .views import (
+
+    mine_transactions,
+    allocate_votes_view,
+    home,
+    create_new_poll_page,
+    PollListView,
+    my_polls_template_view,
+    poll_invites_template_view,
+    PollInvitationsView,
+    invite_users_for_poll,
+    voting_template_view,
+)
 
 app_name = "voting"
 urlpatterns = [
-    path("vote/<int:poll_id>/", vote, name="vote"),
+
     path("mine/", mine_transactions, name="mine_transactions"),
     path("allocate/<int:poll_id>/", allocate_votes_view, name="allocate_votes"),
     path("", home, name="home"),
     path("create/", create_new_poll_page, name="create_new_poll"),
     path("polls/", PollListView.as_view(), name="polls"),
-
+    path("my-polls/", my_polls_template_view, name="my_polls"),
+    path("poll-invites/", poll_invites_template_view, name="poll_invites"),
+    path("poll-invitations/", PollInvitationsView.as_view(), name="poll_invitations"),
+    path(
+        "poll-invitations/<int:id>/",
+        PollInvitationsView.as_view(),
+        name="poll_invitations",
+    ),
+    path("invite/<int:poll_id>/", invite_users_for_poll, name="invite_users_for_poll"),
+    path("voting/<int:poll_id>/", voting_template_view, name="voting"),
 ]
