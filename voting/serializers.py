8d8448e5014c0a6f2ab3afdae785b53550b6cc6e
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-11 15:35:52 +0300

    chore: Update PollSerializer to include PollInvitation model

--- voting/serializers.py
+++ voting/serializers.py
@@ -1,18 +1,35 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import Poll
+from .models import Poll, PollInvitation
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'email']  # Add other fields if needed
+        fields = ["id", "username", "email"]  # Add other fields if needed
 
 
 class PollSerializer(serializers.ModelSerializer):
-    candidates = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all(), many=True, required=False)
+    candidates = serializers.PrimaryKeyRelatedField(
+        queryset=get_user_model().objects.all(), many=True, required=False
+    )
 
     class Meta:
         model = Poll
-        fields = ['id', 'title', 'description', 'start_date', 'end_date', 'created_by', 'candidates', 'avatar']
+        fields = [
+            "id",
+            "title",
+            "description",
+            "start_date",
+            "end_date",
+            "created_by",
+            "avatar",
+        ]
+
+
+class PollInvitationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PollInvitation
+        fields = "__all__"
