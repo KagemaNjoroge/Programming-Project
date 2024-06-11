942cfe4c979dd8fb15b767572621cbfc1d83e137
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-11 15:35:58 +0300

    feat: Add is_active field to Poll model and create PollInvitation model

--- voting/models.py
+++ voting/models.py
@@ -11,9 +11,11 @@ class Poll(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(default=now)
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
 
-    candidates = models.ManyToManyField(get_user_model(), related_name="candidates", blank=True)
-    avatar = models.ImageField(upload_to="polls/", null=True, blank=True, default="polls/default.jpg")
+    avatar = models.ImageField(
+        upload_to="polls/", null=True, blank=True, default="polls/default.jpg"
+    )
 
     def __str__(self):
         return self.title
@@ -23,14 +25,43 @@ class Poll(models.Model):
         verbose_name = "Poll"
 
 
+class PollCandidate(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    number_of_coins = models.IntegerField()
+
+    def __str__(self):
+        return f"Candidate: {self.id}"
+
+    class Meta:
+        verbose_name_plural = "Poll Candidates"
+        verbose_name = "Poll Candidate"
+
+
 class PollWallet(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     balance = models.IntegerField(default=0)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Wallet: {self.id}'
+        return f"Wallet: {self.id}"
 
     class Meta:
         verbose_name_plural = "Poll Wallets"
         verbose_name = "Poll Wallet"
+
+
+class PollInvitation(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    invited_user = models.EmailField(max_length=255, null=True, blank=True)
+    is_accepted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"Invitation: {self.id}"
+
+    class Meta:
+        verbose_name_plural = "Poll Invitations"
+        verbose_name = "Poll Invitation"
